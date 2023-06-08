aeef29f66b01ee50863e6d5a95b8cb2c473808ac
DarthRevenSVK <peterbucek.pb@gmail.com>
2023-06-08 18:15:03 +0200

    skuska dalsiehej zmeny

---
+++ src/heloWorld.java
@@ -0,0 +1,10 @@
+public class heloWorld {
+    public static void main(String[] args) {
+        String meno= "hello WORLD!";
+        System.out.println(meno);
+        System.out.println("Ahooj Swet!");
+
+    }
+
+
+}
