b65cb3f34ad11c38cffab7c8e468d59634cecc90
DarthRevenSVK <peterbucek.pb@gmail.com>
2023-06-10 13:02:08 +0200

    praca z if if els

--- src/Ulohy/PriradenieZnamky.java
+++ src/Ulohy/PriradenieZnamky.java
@@ -1,4 +1,7 @@
 package Ulohy;
+
+import java.util.Scanner;
+
 //Úloha 1: Program na priradenie známky
 //
 //Vytvorte prostredníctvom vstupu z konzoly rozhodovací test, ktorý vypíše známku A, B alebo C podľa týchto kritérií:
@@ -7,4 +10,26 @@ package Ulohy;
 //Ak je percento nad 65, tak vypíše známku C
 //Použite metódu pre rozhodovanie.
 public class PriradenieZnamky {
+
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.print("percenta zo skusky ");
+        int x = console.nextInt();
+
+        if (x > 90) {
+            System.out.print("A");
+        } else if (x > 75) {
+            System.out.print("B");
+        } else if (x > 65) {
+            System.out.print("C");
+        } else
+            System.out.println("neuspel");
+
+
+
+
+    }
 }
+
+
+
